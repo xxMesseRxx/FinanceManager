ec620999e5e609827ef36a78ae95bc9187041318
xxMesseRxx <343454640@mail.ru>
2023-05-04 22:12:52 +0500

    Was created migration.

---
+++ FinanceManager/Migrations/20230504161158_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Operations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsExpense = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Operations_Id", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinancialOperations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sum = table.Column<int>(type: "int", nullable: false),
+                    Discription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OperationTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialOperations_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinancialOperation_Operations",
+                        column: x => x.OperationTypeId,
+                        principalTable: "Operations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinancialOperations_OperationTypeId",
+                table: "FinancialOperations",
+                column: "OperationTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_Operations_Type",
+                table: "Operations",
+                column: "Type",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialOperations");
+
+            migrationBuilder.DropTable(
+                name: "Operations");
+        }
+    }
+}
