00353a0bbbbf7268ee57f1616837ea713b098f5f
xxMesseRxx <343454640@mail.ru>
2023-05-05 21:13:10 +0500

    In tests project was added "ServiceCreator" class

---
+++ FinanceManager.Tests/ServicesGreator.cs
@@ -0,0 +1,27 @@
+﻿namespace FinanceManager.Tests;
+
+using FinanceManager.Model;
+using FinanceManager.Services;
+using Microsoft.EntityFrameworkCore;
+
+public class ServicesGreator
+{
+	private FinanceManagerContext _db;
+
+	public ServicesGreator()
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<FinanceManagerContext>();
+		optionsBuilder.UseSqlServer("Server=localhost;Database=TestFinance;Trusted_Connection=True;Encrypt=False;");
+
+		_db = new FinanceManagerContext(optionsBuilder.Options);
+	}
+
+	public OperationService GetOperationService()
+	{
+		return new OperationService(_db);
+	}
+	public FinancialOperationService GetFinOperService()
+	{
+		return new FinancialOperationService(_db);
+	}
+}
