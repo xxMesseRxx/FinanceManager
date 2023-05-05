ae2f82e76977db1ea7b560d2937aa2cd683d995b
xxMesseRxx <343454640@mail.ru>
2023-05-05 09:55:38 +0500

    In tests project was created new class: "TestDBCreator".

---
+++ FinanceManager.Tests/TestDBCreator.cs
@@ -0,0 +1,24 @@
+﻿namespace FinanceManager.Tests;
+
+using FinanceManager.Model;
+using Microsoft.EntityFrameworkCore;
+
+public class TestDBCreator : IDisposable
+{
+	private FinanceManagerContext? _db;
+
+	public void CreateTestDB()
+	{
+		var optionsBuilder = new DbContextOptionsBuilder<FinanceManagerContext>();
+		optionsBuilder.UseSqlServer("Server=localhost;Database=TestUniversity;Trusted_Connection=True;Encrypt=False;");
+
+		_db = new FinanceManagerContext(optionsBuilder.Options);
+
+		_db?.Database.EnsureCreated();
+	}
+	public void Dispose()
+	{
+		_db?.Database.EnsureDeleted();
+	}
+}
+
