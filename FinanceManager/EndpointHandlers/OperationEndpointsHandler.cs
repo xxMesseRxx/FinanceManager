792e328bfbd1cf44a114685235a912966d3a2980
xxMesseRxx <xxMesseRxx@HOME>
2023-05-08 19:45:35 +0500

    Was created "OperationEndpointsHandler class" with 2 methods: "GetAllAsync" and "GetOperationAsync".

---
+++ FinanceManager/EndpointHandlers/OperationEndpointsHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace FinanceManager.EndpointHandlers;
+
+using FinanceManager.Library.Interfaces;
+using FinanceManager.Model;
+
+public static class OperationEndpointsHandler
+{
+    public static async Task<IResult> GetAllAsync(IOperationService operationService)
+    {
+        List<Operation> operations = await operationService.GetAllAsync();
+
+        return Results.Json(operations);
+    }
+    public static async Task<IResult> GetOperationAsync(IOperationService operationService,
+                                                        int id)
+    {
+        Operation operation = await operationService.GetOperationAsync(id);
+
+        if (operation is null)
+        {
+            return Results.NotFound(new { message = "Операция не найдена" });
+        }
+
+        return Results.Json(operation);
+    }
+}
--- FinanceManager/Program.cs
+++ FinanceManager/Program.cs
@@ -1,6 +1,24 @@
+using FinanceManager.Library.Interfaces;
+using FinanceManager.Model;
+using FinanceManager.Services;
+using Microsoft.EntityFrameworkCore;
+using FinanceManager.EndpointHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string DbConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<FinanceManagerContext>(options => options.UseSqlServer(DbConnection));
+builder.Services.AddTransient<IOperationService, OperationService>();
+builder.Services.AddTransient<ITransactionService, TransactionService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/operations", OperationEndpointsHandler.GetAllAsync);
+app.MapGet("/operations/{id:int}", OperationEndpointsHandler.GetOperationAsync);
+
+app.MapGet("/transactions", TransactionEndpointsHandler.GetAllAsync);
 
 app.Run();
