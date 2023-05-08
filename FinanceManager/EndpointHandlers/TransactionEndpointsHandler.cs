bfd12fdff13d4121bc74de9f18cdbc3707076a7a
xxMesseRxx <xxMesseRxx@HOME>
2023-05-08 20:45:41 +0500

    Was created new class: "TransactionEndpointsHandler".

---
+++ FinanceManager/EndpointHandlers/TransactionEndpointsHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceManager.EndpointHandlers;
+
+using FinanceManager.Library.Interfaces;
+using FinanceManager.Model;
+using FinanceManager.Services;
+
+public static class TransactionEndpointsHandler
+{
+	public static async Task<List<Transaction>> GetAllAsync(ITransactionService transactionService)
+	{
+		return await transactionService.GetAllAsync();
+	}
+}
