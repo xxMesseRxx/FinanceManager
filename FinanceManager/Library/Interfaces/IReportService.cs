d66c9546f153e5517c43adda2a10fd90225515e2
xxMesseRxx <xxMesseRxx@HOME>
2023-05-14 11:50:12 +0500

    Were added new Interface: "IReportService" and his implementation.

---
+++ FinanceManager/Library/Interfaces/IReportService.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManager.Library.Interfaces;
+
+using FinanceManager.Model;
+
+public interface IReportService
+{
+    public Task<DailyReport> GetDailyReportAsync(DateOnly date);
+}
---
+++ FinanceManager/Services/ReportService.cs
@@ -0,0 +1,22 @@
+﻿namespace FinanceManager.Services;
+
+using FinanceManager.Library.Interfaces;
+using FinanceManager.Model;
+using System.Threading.Tasks;
+
+public class ReportService : IReportService
+{
+    private ITransactionService _transactionService;
+
+    public ReportService(ITransactionService transactionService)
+    {
+        _transactionService = transactionService;
+    }
+
+    public async Task<DailyReport> GetDailyReportAsync(DateOnly date)
+    {
+        DailyReport dailyReport = new DailyReport(date, _transactionService);
+
+        return dailyReport;
+    }
+}
