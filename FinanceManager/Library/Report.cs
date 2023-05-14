b7f111966e212bef76cd2bc15e9cd746e09051c0
xxMesseRxx <xxMesseRxx@HOME>
2023-05-14 11:48:30 +0500

    Was added new abstract class: "Report"

---
+++ FinanceManager/Library/Report.cs
@@ -0,0 +1,31 @@
+﻿namespace FinanceManager.Library;
+
+using FinanceManager.Model;
+
+public abstract class Report
+{
+    public List<Transaction> Transactions { get; protected set; }
+    public int TotalIncome { get; protected set; }
+    public int TotalExpenses { get; protected set; }
+
+    protected void CountTotalIncome()
+    {
+        foreach (var transaction in Transactions)
+        {
+            if (transaction.Sum > 0)
+            {
+                TotalIncome += transaction.Sum;
+            }
+        }
+    }
+    protected void CountTotalExpenses()
+    {
+        foreach (var transaction in Transactions)
+        {
+            if (transaction.Sum < 0)
+            {
+                TotalExpenses += transaction.Sum;
+            }
+        }
+    }
+}
