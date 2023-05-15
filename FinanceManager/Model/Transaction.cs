41099d3da5e268a10295c1c864d9a4d470c4c4db
xxMesseRxx <xxMesseRxx@HOME>
2023-05-15 21:42:41 +0500

    Some fix

--- FinanceManager/Model/Transaction.cs
+++ FinanceManager/Model/Transaction.cs
@@ -7,7 +7,6 @@ public class Transaction
     public int Id { get; set; }
     public int Sum { get; set; }
     public string? Discription { get; set; }
-    [DisplayFormat(DataFormatString = "{dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     public DateTime DateTime { get; set; }
 	public int OperationId { get; set; }
     public Operation Operation { get; set; } = null!;
