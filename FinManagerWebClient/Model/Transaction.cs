03f6305591b1db594a6009574bc93f0eb964ab69
xxMesseRxx <xxMesseRxx@HOME>
2023-05-30 20:23:13 +0500

    In model were added 2 class: "Operation" and "Transaction"

---
+++ FinManagerWebClient/Model/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace FinManagerWebClient.Model;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public int Sum { get; set; }
+    public string? Discription { get; set; }
+    public DateTime DateTime { get; set; }
+    public int OperationId { get; set; }
+    public Operation Operation { get; set; }
+}
