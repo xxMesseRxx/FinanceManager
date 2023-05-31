d94cce6095d857be15fa542a0d86f6e73c7cb49a
xxMesseRxx <xxMesseRxx@HOME>
2023-05-31 22:14:56 +0500

    Were created TransactionRequests class and him interface.

---
+++ FinManagerWebClient/RequestServices/TransactionRequests.cs
@@ -0,0 +1,45 @@
+﻿namespace FinManagerWebClient.RequestServices;
+
+using FinManagerWebClient.DTO;
+using FinManagerWebClient.Library.Requests;
+using FinManagerWebClient.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class TransactionRequests : ITransactionRequests
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
+
+    public TransactionRequests(IHttpClientFactory httpClientFactory)
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        _baseUrl = config["URLs:FinanceManager:Transactions"];
+
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public Task CreateAsync(TransactionCreateDto transactionCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<TransactionVM>> GetAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<TransactionVM>>(_baseUrl);
+    }
+
+    public Task<TransactionVM> RemoveAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TransactionVM> UpdateAsync(TransactionUpdateDto transactionUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+}
