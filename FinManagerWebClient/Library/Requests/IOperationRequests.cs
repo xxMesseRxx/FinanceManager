d88c01322074e6dc625b33c362a1eeb9c9d7f475
xxMesseRxx <xxMesseRxx@HOME>
2023-06-01 21:05:26 +0500

    Was created inteface: "IOperationRequests".

---
+++ FinManagerWebClient/Library/Requests/IOperationRequests.cs
@@ -0,0 +1,12 @@
+﻿namespace FinManagerWebClient.Library.Requests;
+
+using FinManagerWebClient.DTO;
+using FinManagerWebClient.Model;
+
+public interface IOperationRequests
+{
+    public Task<List<OperationVM>> GetAsync();
+    public Task CreateAsync(OperationVM operation);
+    public Task<OperationVM> UpdateAsync(OperationVM operation);
+    public Task<OperationVM> RemoveAsync(int id);
+}
