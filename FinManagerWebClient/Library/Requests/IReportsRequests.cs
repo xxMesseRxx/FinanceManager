79b0635fa3e6e3cfa8ec18cd1f2b6c7ccdf0b6b2
xxMesseRxx <xxMesseRxx@HOME>
2023-06-04 15:32:26 +0500

    Were created new interface: "IReportsRequests" and him implementation class: "ReportsRequests".

---
+++ FinManagerWebClient/Library/Requests/IReportsRequests.cs
@@ -0,0 +1,9 @@
+﻿namespace FinManagerWebClient.Library.Requests;
+
+using FinManagerWebClient.DTO.Reports;
+
+public interface IReportsRequests
+{
+    public Task<DailyReport> GetDailyReportAsync(DateTime date);
+    public Task<PeriodReport> GetPeriodReportAsync(DateTime startDate, DateTime endDate);
+}
---
+++ FinManagerWebClient/RequestServices/ReportsRequests.cs
@@ -0,0 +1,38 @@
+﻿namespace FinManagerWebClient.RequestServices;
+
+using FinManagerWebClient.DTO.Reports;
+using FinManagerWebClient.Library.Requests;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class ReportsRequests : IReportsRequests
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _periodReportUrl;
+    private readonly string _dailyReportUrl;
+
+    public ReportsRequests(IHttpClientFactory httpClientFactory)
+    {
+        IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        _dailyReportUrl = config["URLs:FinanceManager:DailyReport"];
+        _periodReportUrl = config["URLs:FinanceManager:PeriodReport"];
+
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public async Task<DailyReport> GetDailyReportAsync(DateTime date)
+    {
+        return await _httpClient.GetFromJsonAsync<DailyReport>(_dailyReportUrl + "?" +
+                                                               date.ToShortDateString());
+    }
+
+    public async Task<PeriodReport> GetPeriodReportAsync(DateTime startDate, DateTime endDate)
+    {
+        return await _httpClient.GetFromJsonAsync<PeriodReport>(_dailyReportUrl + "?" + 
+                                                                $"startDate={startDate}&endDate={endDate}");
+    }
+}
