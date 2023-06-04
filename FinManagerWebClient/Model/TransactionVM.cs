c1cc90156c019f8dd757a76d4d77370ee4b24e8a
xxMesseRxx <xxMesseRxx@HOME>
2023-06-04 14:24:35 +0500

    Sum property was changed from int to double.

--- FinManagerWebClient/Model/TransactionVM.cs
+++ FinManagerWebClient/Model/TransactionVM.cs
@@ -3,7 +3,7 @@
 public class TransactionVM
 {
     public int Id { get; set; }
-    public int Sum { get; set; }
+    public double Sum { get; set; }
     public string? Discription { get; set; }
     public DateTime DateTime { get; set; }
     public int OperationId { get; set; }
